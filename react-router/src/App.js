e94b5f5af605c4bea83d3596c4aa45eed12e41f6
white-hun <exwhite@naver.com>
2023-03-26 20:00:55 +0900

    Update

---
+++ react-router/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Home</p>,
+    errorElement: <p>Not Found</p>,
+  },
+  {
+    path: "/videos",
+    element: <p>Videos</p>,
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
